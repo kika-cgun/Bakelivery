cd6900732663deb45e82d15d8499e94b55a1d2d9
Piotr Capecki <148986150+kika-cgun@users.noreply.github.com>
2026-04-27 11:47:47 +0200

    feat: initialize Spring Boot backend project with Gradle and required dependencies

---
+++ backend/src/main/java/com/piotrcapecki/bakelivery/BakeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.piotrcapecki.bakelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeliveryApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/piotrcapecki/bakelivery/BakeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrcapecki.bakelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
