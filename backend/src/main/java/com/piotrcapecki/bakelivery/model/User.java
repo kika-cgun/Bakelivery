770d00300e3603e07e1f2d9a8cbb800dda99bc73
Piotr Capecki <148986150+kika-cgun@users.noreply.github.com>
2026-04-27 20:13:01 +0200

    fix: add @GeneratedValue to User entity UUID primary key

--- backend/src/main/java/com/piotrcapecki/bakelivery/model/User.java
+++ backend/src/main/java/com/piotrcapecki/bakelivery/model/User.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 public class User implements UserDetails {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(unique = true, nullable = false)
